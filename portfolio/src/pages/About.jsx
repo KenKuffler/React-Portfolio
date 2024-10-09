01a415e96305e0bb062545d4d0ca90524e9935fb
KenKuffler <kennethrkuffler@hotmail.com>
2024-10-08 19:28:29 -0500

    added photo of me to about page

--- portfolio/src/pages/About.jsx
+++ portfolio/src/pages/About.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import '../styles/About.css';
+import myImage from '../assets/me.jpeg';
 
 function About() {
   return (
     <section className="about">
+      <div id="photo">
+        <img src={myImage} alt="Kenneth Kuffler" />
+      </div>
       <h1>Hi, I'm Kenneth Kuffler</h1>
       <h2>I'm a Web Developer</h2>
       <p>
